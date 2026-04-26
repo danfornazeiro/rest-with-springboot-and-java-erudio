38c8bee56a0fc5b5cb49e5d945304c49dd82b361
nelsonfornazeiro <nelsonfornazeiro@gmail.com>
2026-04-26 14:42:50 -0300

    feat: add methods to create directory and try to store the file in the directory

--- src/main/java/com/frnz7/restSpring/service/FileStorageService.java
+++ src/main/java/com/frnz7/restSpring/service/FileStorageService.java
@@ -1,4 +1,50 @@
 package com.frnz7.restSpring.service;
 
+import com.frnz7.restSpring.config.FileStorageConfig;
+import com.frnz7.restSpring.exception.FileStorageException;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@Service
 public class FileStorageService {
+
+    private final Path fileStorageLocation;
+
+    public FileStorageService(FileStorageConfig fileStorageConfig) {
+        Path path = Paths.get(fileStorageConfig.getUploadDir())
+                .toAbsolutePath()
+                .normalize();
+        this.fileStorageLocation = path;
+
+        try{
+            Files.createDirectories(this.fileStorageLocation);
+        } catch (Exception e) {
+            throw new FileStorageException("Couldn't create the directory where files will be stored!", e);
+        }
+    }
+
+    public String storeFile(MultipartFile file){
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+
+        try{
+            if(fileName.contains("..")){
+                throw new FileStorageException("Sorry! File name contains a invalid path sequence " + fileName);
+            }
+
+            Path targetLocation = this.fileStorageLocation.resolve(fileName);
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+            return fileName;
+        } catch (Exception e) {
+            throw new FileStorageException("Couldn't store file " + fileName + ". Please try again!",e);
+        }
+
+    }
+
 }
