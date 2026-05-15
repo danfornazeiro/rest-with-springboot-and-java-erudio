2f7767bbdf50877da4e96d8398adaae3c4611a23
nelsonfornazeiro <nelsonfornazeiro@gmail.com>
2026-05-15 16:22:34 -0300

    feat: add controller to send email

---
+++ src/main/java/com/frnz7/restSpring/controller/EmailController.java
@@ -0,0 +1,36 @@
+package com.frnz7.restSpring.controller;
+
+import com.frnz7.restSpring.controller.docs.EmailControllerDocs;
+import com.frnz7.restSpring.data.dto.request.EmailRequestDTO;
+import com.frnz7.restSpring.service.EmailService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/email/v1")
+public class EmailController implements EmailControllerDocs {
+
+    private final EmailService service;
+
+    public EmailController(EmailService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    @Override
+    public ResponseEntity<String> sendEmail(@RequestBody EmailRequestDTO emailRequestDTO) {
+        service.sendSimpleEmail(emailRequestDTO);
+        return new ResponseEntity<>("e-mail sent with success!", HttpStatus.OK);
+
+    }
+
+    @Override
+    public ResponseEntity<String> sendEmailWithAttachment(String emailRequestJson, MultipartFile multipartFile) {
+        return null;
+    }
+}
--- src/main/java/com/frnz7/restSpring/service/EmailService.java
+++ src/main/java/com/frnz7/restSpring/service/EmailService.java
@@ -1,6 +1,7 @@
 package com.frnz7.restSpring.service;
 
 import com.frnz7.restSpring.config.EmailConfig;
+import com.frnz7.restSpring.data.dto.request.EmailRequestDTO;
 import com.frnz7.restSpring.mail.EmailSender;
 import org.springframework.stereotype.Service;
 
@@ -15,11 +16,11 @@ public class EmailService {
         this.emailConfig = emailConfig;
     }
 
-    public void sendSimpleEmail(String to, String subject, String body){
+    public void sendSimpleEmail(EmailRequestDTO emailRequestDTO){
         emailSender
-                .To(to)
-                .withSubject(subject)
-                .withMessage(body)
+                .To(emailRequestDTO.getTo())
+                .withSubject(emailRequestDTO.getSubject())
+                .withMessage(emailRequestDTO.getBody())
                 .send(emailConfig);
     }
 
